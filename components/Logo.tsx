8f466389db7a9cb37abe76cf6107eeacfcbb37eb
Kristoffer Stobbe <krisstobbe@yahoo.com>
2024-06-06 11:40:21 -0500

    (Bug) - Fix Home Navigation

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,14 +2,11 @@
 
 import React, { FunctionComponent } from 'react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { initial, animate, exit, transition } from 'utils/motions'
 import { SITE_ROUTES, SITE_STRINGS } from '../constants'
 
 export const Logo: FunctionComponent = () => {
-  const pathname = usePathname()
-
   return (
     <LazyMotion features={domAnimation}>
       <m.h3
@@ -19,10 +16,7 @@ export const Logo: FunctionComponent = () => {
         exit={exit}
         transition={transition}
       >
-        <Link
-          href={pathname === SITE_ROUTES.projects ? SITE_ROUTES.home : ''}
-          aria-label="Go to home page"
-        >
+        <Link href={SITE_ROUTES.home} aria-label="Go to home page">
           {SITE_STRINGS.textLogo}
         </Link>
       </m.h3>
