8b6db9fa6182aa399018e8741641ad48a4431e47
Kristoffer Stobbe <krisstobbe@yahoo.com>
2024-07-18 11:34:39 -0500

    Add new project to project history

--- app/projects/components/Filter.tsx
+++ app/projects/components/Filter.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react'
 import { LazyMotion, domAnimation, useInView } from 'framer-motion'
-import { TbBrandJavascript, TbBrandNextjs } from 'react-icons/tb'
+import { TbBrandTypescript, TbBrandNextjs, TbBrandNodejs } from 'react-icons/tb' 
 import { FaReact } from 'react-icons/fa'
 import { FilterButton } from './FilterButton'
 
-export type FilterOption = 'React' | 'Next.js' | 'JavaScript' | undefined
+export type FilterOption = 'TypeScript' | 'React' | 'Next.js' | 'Node.js' | undefined 
 
 interface FilterProps {
   onClick?: (filter: FilterOption) => void
@@ -72,11 +72,18 @@ export function Filter({ onClick = (f) => f }: FilterProps) {
             <TbBrandNextjs size={20} />
           </FilterButton>
           <FilterButton
-            onClick={() => handleFilterClick('JavaScript')}
-            label="JavaScript"
-            active={activeFilter === 'JavaScript'}
+            onClick={() => handleFilterClick('TypeScript')}
+            label="TypeScript"
+            active={activeFilter === 'TypeScript'}
           >
-            <TbBrandJavascript size={20} />
+            <TbBrandTypescript size={20} />
+          </FilterButton>
+          <FilterButton
+            onClick={() => handleFilterClick('Node.js')}
+            label="Node.js"
+            active={activeFilter === 'Node.js'}
+          >
+            <TbBrandNodejs size={20} />
           </FilterButton>
         </div>
       </div>
